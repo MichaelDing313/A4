f3b7e4548f427b57da22aaeaaf54892ab1fbe4e3
Michael <michael.ding313@gmail.com>
2017-04-04 11:59:46 -0400

    cartpole works

--- cartpole.py
+++ cartpole.py
@@ -37,71 +37,37 @@ env.seed(RNG_SEED)
 
 # Parameters for single layer NN as policy network
 hidden_size = 8
-alpha = 0.01
+alpha = 0.001
 TINY = 1e-8
-gamma = 0.98
+gamma = 0.99
 
-# Initialize NN weights for polity
+# Initialize NN weights for policy
 weights_init = xavier_initializer(uniform=False)
 relu_init = tf.constant_initializer(0.1)
 
 # If loading of previous model is selected, do that, if not use initialized weights
 if args.load_model:
     model = np.load(args.load_model)
-    hw_init = tf.constant_initializer(model['hidden/weights'])
-    hb_init = tf.constant_initializer(model['hidden/biases'])
-    mw_init = tf.constant_initializer(model['mus/weights'])
-    mb_init = tf.constant_initializer(model['mus/biases'])
     sw_init = tf.constant_initializer(model['sigmas/weights'])
     sb_init = tf.constant_initializer(model['sigmas/biases'])
 else:
-    hw_init = weights_init
-    hb_init = relu_init
-    mw_init = weights_init
-    mb_init = relu_init
     sw_init = weights_init
     sb_init = relu_init
-
-# Get the number of outputs of NN to corrosponse to the number of inputs to model
-try:
-    output_units = env.action_space.shape[0]
-except AttributeError:
-    output_units = env.action_space.n
 
 # Number of inputs to the nerual net, this is the number of observation outputs
 # The model have
 input_shape = env.observation_space.shape[0]
 
-# Number of inputs, for use in tensorflow
-NUM_INPUT_FEATURES = 4
-
 # Tensorflow input and output initialization
-x = tf.placeholder(tf.float32, shape=(None, NUM_INPUT_FEATURES), name='x')
-y = tf.placeholder(tf.float32, shape=(None, 1), name='y')
+# Input features: 4, output features 2
+x = tf.placeholder(tf.float32, shape=(None, 4), name='x')
+y = tf.placeholder(tf.float32, shape=(None, 2), name='y')
 
-# Layers of the policy network
-hidden = fully_connected(
-    inputs=x,
-    num_outputs=hidden_size,
-    activation_fn=tf.nn.relu,
-    weights_initializer=hw_init,
-    weights_regularizer=None,
-    biases_initializer=hb_init,
-    scope='hidden')
-
-mus = fully_connected(
-    inputs=hidden,
-    num_outputs=output_units,
-    activation_fn=tf.tanh,
-    weights_initializer=mw_init,
-    weights_regularizer=None,
-    biases_initializer=mb_init,
-    scope='mus')
-
+# Olny one lay network is required since the system is quite simple
 sigmas = tf.clip_by_value(fully_connected(
-    inputs=hidden,
-    num_outputs=output_units,
-    activation_fn=tf.nn.softplus,
+    inputs=x,
+    num_outputs=2,
+    activation_fn=tf.nn.sigmoid,
     weights_initializer=sw_init,
     weights_regularizer=None,
     biases_initializer=sb_init,
@@ -112,8 +78,9 @@ sigmas = tf.clip_by_value(fully_connected(
 all_vars = tf.global_variables()
 
 # Layers of network
-pi = tf.contrib.distributions.Normal(mus, sigmas, name='pi')
-pi_sample = tf.argmax(tf.reduce_mean(tf.tanh(pi.sample(), name='pi_sample'),0),0)
+# Use bernulie distribution here instead since actions are descrete
+pi = tf.contrib.distributions.Bernoulli(p = sigmas, name='pi')
+pi_sample = pi.sample()
 
 log_pi = pi.log_prob(y, name='log_pi')
 
@@ -132,6 +99,12 @@ MAX_STEPS = env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps')
 
 # Run the enviorment
 track_returns = []
+plt_returns = []
+plt_avg_returns = []
+plt_sw = []
+plt_x = []
+
+
 for ep in range(16384):
     obs = env.reset()
 
@@ -145,9 +118,11 @@ for ep in range(16384):
     while not done:
         ep_states.append(obs)
         env.render()
-        action =  sess.run([pi_sample], feed_dict={x:[obs]})[0]
-        ep_actions.append([action])
-        obs, reward, done, info = env.step(action)
+        action =  sess.run([pi_sample], feed_dict={x:[obs]})[0][0]
+    
+        obs, reward, done, info = env.step(np.argmax(list(action)))
+        
+        ep_actions.append(action)
         ep_rewards.append(reward * I)
         G += reward * I
         I *= gamma
@@ -159,8 +134,6 @@ for ep in range(16384):
     if not args.load_model:
         returns = np.array([G - np.cumsum(ep_rewards[:-1])]).T
         index = ep % MEMORY
-        
-        
         _ = sess.run([train_op],
                     feed_dict={x:np.array(ep_states),
                                 y:np.array(ep_actions),
@@ -169,13 +142,28 @@ for ep in range(16384):
     track_returns.append(G)
     track_returns = track_returns[-MEMORY:]
     mean_return = np.mean(track_returns)
+    plt_avg_returns.append(mean_return)
+    plt_returns.append(G)
+
+    with tf.variable_scope('sigmas', reuse=True):
+        plt_sw.append(sess.run(tf.get_variable('weights')))
+        #plt_sb.append(sess.run(tf.get_variable('bias')))
     print("Episode {} finished after {} steps with return {}".format(ep, t, G))
     print("Mean return over the last {} episodes is {}".format(MEMORY,
                                                                mean_return))
 
-
-    with tf.variable_scope("mus", reuse=True):
-        print("incoming weights for the mu's from the first hidden unit:", sess.run(tf.get_variable("weights"))[0,:])
-
-
 sess.close()
+
+try:
+    plt.figure()
+    plt.plot(plot_x, plot_train, '-g', label='Training')
+    plt.plot(plot_x, plot_vali, '-r', label='Validation')
+    plt.plot(plot_x, plot_test, '-b', label='Test')
+    plt.xlabel("Training Iterations")
+    plt.ylabel("Accuracy")
+    plt.title("Traning Curve for Single Hidden Layer NN, lam = {}".format(lam))
+    plt.legend(loc='bottom right')
+    plt.show()
+except:
+    print("plot fail")
+        
